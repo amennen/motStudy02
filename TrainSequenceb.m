2f2e1b64db5004e8b476eb504352676fd75f5d36
normanlab <amennen@princeton.edu>
2016-09-15 10:06:05 -0400

    subject 16 day 1

--- TrainSequenceb.m
+++ TrainSequenceb.m
@@ -1,7 +1,7 @@
 base_path = [fileparts(which('mot_realtime01.m')) filesep];
 cd(base_path);
 
-SUBJECT = 200;
+SUBJECT = 16;
 SVEC = 8:15;
 
 NUM_TASK_RUNS = 3;
@@ -40,12 +40,12 @@ ASSOCIATES = RECALL2 + 1;
 mot_realtime01b(SUBJECT, SETUP, [], 0, 0);
 
 %for testing
-mot_realtime01b(SUBJECT,TOCRITERION1,1,0,0);
+%mot_realtime01b(SUBJECT,TOCRITERION1,[],0,0);
 % this will continue to train test and practice MOT, then move on to
 % MOT_Practice, MOT_PREP
 s2 = findMatch(SUBJECT,SVEC);
 
-mot_realtime01b(SUBJECT,FAMILIARIZE2, 1, 0, 0,s2); %continue because want to not go through the break
+%mot_realtime01b(SUBJECT,FAMILIARIZE2, 1, 0, 0,s2); %continue because want to not go through the break
 
 mot_realtime01b(SUBJECT, FAMILIARIZE2, [], 0, 0,s2); %continue because want to not go through the break
 
