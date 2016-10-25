70504b230c1abda9e98425eb621d61a77539e93b
Anne <amennen@princeton.edu>
2016-10-25 15:16:48 -0400

    last min changes

---
+++ compareFeedback.m
@@ -0,0 +1,76 @@
+% compare feedback
+
+close all;
+clear all;
+projectName = 'motStudy02';
+allspeeds = [];
+allsep = [];
+nstim = 10;
+nTRs = 15;
+sepTRs = 17;
+FBTRs = 11;
+nblock = 3;
+
+svec = [8 12:16 18 20:22 24 26 27];
+RT = [8 12:15 18 21 22];
+YC = [16 20 24 26 27];
+iRT = find(ismember(svec,RT));
+iYC = find(ismember(svec,YC));
+
+nsub = length(svec);
+sepbystim = zeros(nstim,nTRs*3);
+speedbystim = zeros(nstim,nTRs*3);
+allplotDir = ['/Data1/code/' projectName '/' 'Plots' '/' ];
+
+for s = 1:nsub
+    subjectNum = svec(s);
+    for iblock = 1:nblock
+        blockNum = iblock;
+        SESSION = 19 + blockNum;
+        
+        behavioral_dir = [fileparts(which('mot_realtime01.m')) '/BehavioralData/' num2str(subjectNum) '/'];
+        save_dir = ['/Data1/code/' projectName '/data/' num2str(subjectNum) '/']; %this is where she sets the save directory!
+        runHeader = fullfile(save_dir,[ 'motRun' num2str(blockNum) '/']);
+        fileSpeed = dir(fullfile(behavioral_dir, ['mot_realtime01_' num2str(subjectNum) '_' num2str(SESSION)  '*.mat']));
+        names = {fileSpeed.name};
+        dates = [fileSpeed.datenum];
+        [~,newest] = max(dates);
+        plotDir = ['/Data1/code/' projectName '/' 'Plots' '/' num2str(subjectNum) '/'];
+        if ~exist(plotDir, 'dir')
+            mkdir(plotDir);
+        end
+        matlabOpenFile = [behavioral_dir '/' names{newest}];
+        d = load(matlabOpenFile);
+        allSpeed = d.stim.motionSpeed; %matrix of TR's
+        speedVector = reshape(allSpeed,1,numel(allSpeed));
+        allMotionTRs = convertTR(d.timing.trig.wait,d.timing.plannedOnsets.motion,d.config.TR); %row,col = mTR,trialnumber
+        allMotionTRs = allMotionTRs + 2;%[allMotionTRs; allMotionTRs(end,:)+1; allMotionTRs(end,:) + 2]; %add in the next 2 TR's for HDF
+        onlyFbTRs = allMotionTRs(4:end,:);
+        TRvector = reshape(allMotionTRs,1,numel(allMotionTRs));
+        FBTRVector = reshape(onlyFbTRs,1,numel(onlyFbTRs));
+        run = dir([runHeader 'motpatternsdata_' num2str(SESSION) '*']);
+        names = {run.name};
+        dates = [run.datenum];
+        [~,newest] = max(dates);
+        run = load(fullfile(runHeader,run(end).name));
+        categsep = run.patterns.categsep(TRvector - 10); %minus 10 because we take out those 10
+        sepbytrial = reshape(categsep,nTRs,10);
+        allsepchange = diff(sepbytrial,1,1);
+        FBsepchange = reshape(allsepchange(4:end,:),1,numel(allsepchange(4:end,:)));
+        allspeedchanges = diff(d.stim.motionSpeed,1,1);
+        FBspeedchange = reshape(allspeedchanges(4:end,:),1,numel(allspeedchanges(4:end,:)));
+        FBTRs = length(FBspeedchange);
+        ds((iblock-1)*FBTRs + 1: iblock*FBTRs ,s) = FBspeedchange;
+        ev((iblock-1)*FBTRs + 1: iblock*FBTRs ,s) = FBsepchange%FBsep;
+    end
+    
+figure;
+plot(ds(:,s),ev(:,s), '.')
+end
+
+figure
+for i = 1:s
+%figure;
+hold on
+plot(ds(:,i),ev(:,i), '.')
+end
